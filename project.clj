e4c84585c6f73348667b11b1bb2ec8eb6d0d33d9
divyum <divyum.rastogi@webklipper.com>
2017-09-29 03:25:35 +0530

    show current time

--- project.clj
+++ project.clj
@@ -5,5 +5,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [http-kit "2.2.0"]]
+                 [http-kit "2.2.0"]
+                 [clj-time "0.14.0"]]
   :main clojure-simple-http.core)
--- src/clojure_simple_http/core.clj
+++ src/clojure_simple_http/core.clj
@@ -1,10 +1,11 @@
 (ns clojure-simple-http.core
-  (:require [org.httpkit.server :refer [run-server]]))
+  (:require [org.httpkit.server :refer [run-server]]
+            [clj-time.core :as t]))
 
 (defn app [req]
   {:status  200
    :headers {"Content-Type" "text/html"}
-   :body    "hello world!"})
+   :body    (str (t/time-now))})
 
 (defn -main [& args]
   (run-server app {:port 8080})
