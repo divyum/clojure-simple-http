90bf913f58b76679f2d453cac2b23626b5bb030b
divyum <divyum.rastogi@webklipper.com>
2017-09-29 02:49:03 +0530

    minimal server

--- src/clojure_simple_http/core.clj
+++ src/clojure_simple_http/core.clj
@@ -8,16 +8,6 @@
    :headers {"Content-Type" "text/html"}
    :body    "hello world!"})
 
-(defonce server (atom nil))
-
-(defn stop-server []
-  (when-not (nil? @server)
-    ;; graceful shutdown: wait 100ms for existing requests to be finished
-    ;; :timeout is optional, when no timeout, stop immediately
-    (@server :timeout 100)
-    (reset! server nil)
-    (println "Server stopped")))
-
 (defn -main [& args]
-  (reset! server (run-server #'app {:port 8080}))
-  (println server " Server started on port 8080"))
+  (run-server app {:port 8080})
+  (println "Server started on port 8080"))
