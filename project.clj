bd5e2be82c6a4dc8798222300ea46794e4b6f7a9
divyum <divyum.rastogi@webklipper.com>
2017-10-01 03:58:43 +0530

    adding routes

--- project.clj
+++ project.clj
@@ -6,5 +6,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [http-kit "2.2.0"]
-                 [clj-time "0.14.0"]]
+                 [clj-time "0.14.0"]
+                 [compojure "1.6.0"]]
   :main clojure-simple-http.core)
--- src/clojure_simple_http/core.clj
+++ src/clojure_simple_http/core.clj
@@ -1,11 +1,21 @@
 (ns clojure-simple-http.core
   (:require [org.httpkit.server :refer [run-server]]
-            [clj-time.core :as t]))
+            [clj-time.core :as t]
+            [compojure.core :refer :all]
+            [compojure.route :as route]))
 
-(defn app [req]
-  {:status  200
-   :headers {"Content-Type" "text/html"}
-   :body    (str (t/time-now))})
+(defn get-time
+  []
+  (let [response {:status  200
+                  :headers {"Content-Type" "text/html"}
+                  :body    (str (t/time-now))}]
+    response))
+
+(defroutes app
+           (GET "/" [] "<h1>Welcome</h1>")
+           (GET "/get-time" [] (get-time))
+           (route/not-found "<h1>Page not found</h1>"))
+
 
 (defn -main [& args]
   (run-server app {:port 8080})
