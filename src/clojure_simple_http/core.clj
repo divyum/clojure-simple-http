d4f5b1be93c346b11e0d983bc44ae2f46992685c
Divyum Rastogi <divyumr@amazon.com>
2018-10-27 18:09:03 +0530

    get current date

--- src/clojure_simple_http/core.clj
+++ src/clojure_simple_http/core.clj
@@ -3,6 +3,7 @@
             [clj-time.core :as t]
             [compojure.core :refer :all]
             [compojure.route :as route]))
+(import java.util.Date)
 
 ; returns current time.
 (defn get-time
@@ -12,10 +13,19 @@
                   :body    (str (t/time-now))}]
     response))
 
+; returns current date.
+(defn get-date
+  [greet-name]
+  (let [response {:status  200
+                  :headers {"Content-Type" "text/html"}
+                  :body    (str (.getTime (java.util.Date.)))}]
+    response))
+
 ; define routes.
 (defroutes app
            (GET "/" [] "<h1>Welcome</h1>")
            (GET "/get-time" [] (get-time))
+           (GET "/get-date" [] (get-date))
            (route/not-found "<h1>Page not found</h1>"))
 
 ; starting point of server.
